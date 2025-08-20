e457b51f108532b75bc30f2c8af4db85da42f691
MD. SAKIBUL ISLAM <mdsakibulislam736@gmail.com>
2025-08-21 00:08:37 +0600

    Create HackerRank SQL Basic Solution Bangla Weather Observation Problem 5  Different way solution SQL.sql

---
+++ HackerRank SQL Basic Solution Bangla Weather Observation Problem 5  Different way solution SQL.sql
@@ -0,0 +1,15 @@
+
+
+-- --->Query the two cities in STATION with the shortest
+-- and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is 
+--   more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+
+/*
+Enter your query here.
+*/
+SELECT CITY, LENGTH(CITY) AS LEN FROM STATION
+ORDER BY LENGTH(CITY) ASC, CITY ASC LIMIT 1;
+
+SELECT CITY, LENGTH(CITY) AS LEN FROM STATION
+ORDER BY LENGTH(CITY) DESC, CITY ASC
+LIMIT 1;
